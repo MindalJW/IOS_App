bb6e53e8a8162c444909aeb25c5d277f9e6f2a46
강지원 <seawon1249@naver.com>
2022-12-30 21:51:49 +0900

    second commit

--- Diary/Diary/Assets.xcassets/WriteDiaryViewController.swift
+++ Diary/Diary/Assets.xcassets/WriteDiaryViewController.swift
@@ -34,6 +34,7 @@ class WriteDiaryViewController: UIViewController {
         self.datePicker.preferredDatePickerStyle = .wheels
         self.datePicker.addTarget(self, action:#selector(datePickerValueDidChange(_:)), for: .valueChanged)
         //특정 이벤트가 발생할때마다 내가 작성한 메서드를 동작하도록 만들수있는 메서드
+        //#selector( ) 메서드는 파라미터에 Function Notation 방식으로 함수를 전달합니다
         self.dateTextField.inputView = self.datePicker//키보드 대신 datePicker가 나오게함
     }
     
@@ -45,6 +46,7 @@ class WriteDiaryViewController: UIViewController {
         formmater.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
         formmater.locale = Locale(identifier: "ko_KR")//한국어
         self.diaryDate = datePicker.date//변수에 저장
-        self.dateTextField.text = formmater.string(from: datePicker.date)//string형태로 포맷된 날짜를 변경하여 textfield 입력
+        self.dateTextField.text = formmater.string(from: datePicker.date)
+        //string형태로 포맷된 날짜를 변경하여 textfield 입력
     }
 }
