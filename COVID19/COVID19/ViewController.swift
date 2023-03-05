0ea819d54134bc1bf8a9f33059f30f94179e45c2
강지원 <seawon1249@naver.com>
2023-03-05 23:05:08 +0900

    3/5

---
+++ COVID19/COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 강지원 on 2023/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var commiti3_5: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
